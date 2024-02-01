d8118afd85166045d701afd959981a278c5db51a
Daxit712 <148968892+Daxit712@users.noreply.github.com>
2024-02-01 07:10:40 +0000

    Updated Backend URL.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   firebase: {
-    apiUrl: 'http://backend.safetyforyou.in/api/',
+    apiUrl: 'https://backend.safetyforyou.in/api/',
     projectId: 'safety-signs-d4380',
     appId: '1:493124336288:web:40e3cc259c7c333f67e7c5',
     storageBucket: 'safety-signs-d4380.appspot.com',
